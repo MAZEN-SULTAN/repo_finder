3aa1fc7f624ee5dcce78208d0a38bb87d089c336
maznen sultan <150553345+MAZEN-SULTAN@users.noreply.github.com>
2024-03-18 23:16:33 +0300

    Delete elements.js

--- elements.js
+++
@@ -1,7 +0,0 @@
- export const buttonElement = document.querySelector(".button");
- export const searchInput = document.querySelector(".input");
- export const UserInput = document.querySelector("input[value='users']");
-
- export const cardElement = document.querySelector(".cards");
- export const messageElement = document.querySelector(".message");
- export const LoadElement = document.querySelector(".loader");
