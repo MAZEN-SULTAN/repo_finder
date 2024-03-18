3e65a06b5efe2ff9a9483d070bb98636290f32c2
maznen sultan <150553345+MAZEN-SULTAN@users.noreply.github.com>
2024-03-18 23:16:54 +0300

    Delete setLoadElment.js

--- setLoadElment.js
+++
@@ -1,7 +0,0 @@
-import { LoadElement } from "./elements";
-import { setSearchResult } from "./setSearchResult";
-
-export default function ( loadedState) {
-    LoadElement.classList.toggle("hidden", !loadedState);
-    loadedState && setSearchResult(null);
-}
